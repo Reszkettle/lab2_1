56eab8a7fa9192299e5af85b705c700a5bb32076
Reszkettle <reszkettle@gmail.com>
2021-03-15 18:25:08 +0100

    testy_v1 Added test for finding element when sequence contains only this element, fixed wrong position setting in search method.

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -5,15 +5,26 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 class BinarySearchTest {
 
-    @BeforeEach
-    void setUp() throws Exception {}
+    @BeforeEach void setUp() throws Exception {
+    }
 
-    @Test
-    void test() {
+    @Test void test() {
         fail("Not yet implemented");
+    }
+
+    @Test void shouldFindElementWhenSequenceContainsOnlyThisElement() {
+        // given
+        int element = 100;
+        int[] sequence = {element};
+
+        // when
+        SearchResult result = BinarySearch.search(element, sequence);
+
+        // then
+        assertTrue(result.isFound(), "Expected element should be found");
+        assertEquals(0, result.getPosition(), "Position of found element should be 0");
     }
 
 }
